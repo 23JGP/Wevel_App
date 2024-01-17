538e50e0aa25f75d433d21c2f06241855ca701b7
rkdbs <w2207@e-mirim.hs.kr>
2024-01-17 14:16:11 +0900

    ✨[feat] : index.html main 완성

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// prograss-bar
+const percentageElement = document.getElementById('percentage');
+const progressBarElement = document.getElementById('progress');
+
+const percentageValue = parseFloat(percentageElement.innerText);
+
+progressBarElement.style.width = percentageValue + '%';
+
+// check
+const checkElements = document.querySelectorAll('.check');
+
+checkElements.forEach(checkElement => {
+    checkElement.addEventListener('click', function () {
+        const currentSrc = checkElement.getAttribute('src');
+        const checkImagePath = '../img/check.png';
+        const checkNotImagePath = '../img/check-not.png';
+
+        if (currentSrc === checkNotImagePath)
+            checkElement.setAttribute('src', checkImagePath);
+        else
+            checkElement.setAttribute('src', checkNotImagePath);
+    });
+});
