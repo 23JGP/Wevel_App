ff1a1a1d575fc71dc8fb501e2bd64f2b77df3582
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-23 09:11:26 +0900

    ✨[feat] : tesseract를 이용해서 사진을 스캔하거나 파일을 올려 이미지를 택스토로 변환

--- js/scan.js
+++ js/scan.js
@@ -1,18 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    startWebcam();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   startWebcam();
+// });
 
-function startWebcam() {
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function(stream) {
-            var video = document.getElementById('webcam');
-            if (video) {
-                video.srcObject = stream;
-            } else {
-                console.error('Video element not found.');
-            }
-        })
-        .catch(function(error) {
-            console.error('Error accessing webcam:', error);
-        });
-}
+// function startWebcam() {
+//   navigator.mediaDevices
+//     .getUserMedia({ video: true })
+//     .then(function (stream) {
+//       var video = document.getElementById("webcam");
+//       if (video) {
+//         video.srcObject = stream;
+//       } else {
+//         console.error("Video element not found.");
+//       }
+//     })
+//     .catch(function (error) {
+//       console.error("Error accessing webcam:", error);
+//     });
+// }
