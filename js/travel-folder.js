13c5f8998ec3119ec0e21c9b0a19e9365d10f6e9
rkdbs <w2207@e-mirim.hs.kr>
2024-04-16 19:59:45 +0900

    Comment: input date 주석 삭제

--- js/travel-folder.js
+++ js/travel-folder.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     let lastSelectedNation = null;
     let nameInput = document.getElementById('name-input');
-    let dateInputs = document.querySelectorAll('#date-container input');
     const nationImgs = document.querySelectorAll('.nation-img');
     
     // .nation-img의 마지막 선택자 저장
@@ -30,13 +29,6 @@ document.addEventListener('DOMContentLoaded', function () {
         updateFooterStyle();
     });
 
-    dateInputs.forEach(function (dateInput) {
-        dateInput.addEventListener('input', function () {
-            updateFooterStyle();
-            updateDateInputStyle(dateInput);
-        });
-    });
-
     // 여행 이름, 여행 국가, 기간 모두 다 입력하면 footer 색 변경
     function updateFooterStyle() {
         const isNameInputFilled = nameInput.value.trim() !== '';
@@ -49,20 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
             footer.style.backgroundColor = '#ED4B62';
         } else {
             footer.style.backgroundColor = '#F79FAB';
-        }
-    }
-
-    // date input에 값 지정하면 style 적용
-    function updateDateInputStyle(dateInput) {
-        const value = dateInput.value;
-        if (value.trim() !== '') {
-            dateInput.style.color = '#000'; 
-            dateInput.style.textAlign = 'center';
-            dateInput.style.fontFamily = 'Pretendard';
-        } else {
-            dateInput.style.color = ''; 
-            dateInput.style.textAlign = ''; 
-            dateInput.style.fontFamily = '';
         }
     }
 });
