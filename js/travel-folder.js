4374bc9a4551c8e153ff5465c189133203298221
mic050r <s2216@e-mirim.hs.kr>
2024-06-11 12:09:04 +0900

    ✨ [feat] : 여행 폴더 추가 API연동에 날짜 입력값 추가

--- js/travel-folder.js
+++ js/travel-folder.js
@@ -1,52 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let lastSelectedNation = null;
-    let nameInput = document.getElementById('name-input');
-    const nationImgs = document.querySelectorAll('.nation-img');
-    
-    // .nation-img의 마지막 선택자 저장
-    nationImgs.forEach(function (nationImg) {
-        nationImg.addEventListener('click', function () {
-            if (lastSelectedNation) {
-                lastSelectedNation.classList.remove('active');
-            }
-            
-            nationImg.classList.add('active');
-            
-            lastSelectedNation = nationImg;
-            
-            document.querySelectorAll('.nation-title').forEach(function (title) {
-                title.style.color = 'var(--Gray-04, #707174)';
-            });
+document.addEventListener("DOMContentLoaded", function () {
+  let lastSelectedNation = null;
+  let nameInput = document.getElementById("name-input");
+  const nationImgs = document.querySelectorAll(".nation-img");
 
-            const nationTitle = nationImg.nextElementSibling;
-            nationTitle.style.color = 'var(--Primary-main, #ED4B62)';
+  // .nation-img의 마지막 선택자 저장
+  nationImgs.forEach(function (nationImg) {
+    nationImg.addEventListener("click", function () {
+      if (lastSelectedNation) {
+        lastSelectedNation.classList.remove("active");
+      }
 
-            updateFooterStyle();
-        });
+      nationImg.classList.add("active");
+
+      lastSelectedNation = nationImg;
+
+      document.querySelectorAll(".nation-title").forEach(function (title) {
+        title.style.color = "var(--Gray-04, #707174)";
+      });
+
+      const nationTitle = nationImg.nextElementSibling;
+      nationTitle.style.color = "var(--Primary-main, #ED4B62)";
+
+      updateFooterStyle();
     });
+  });
 
-    nameInput.addEventListener('input', function () {
-        updateFooterStyle();
+  nameInput.addEventListener("input", function () {
+    updateFooterStyle();
+  });
+
+  // 날짜 입력 필드에 변경 이벤트 리스너 추가
+  const dateInputs = document.querySelectorAll(".date");
+  dateInputs.forEach(function (input) {
+    input.addEventListener("change", function () {
+      updateFooterStyle();
     });
+  });
 
-    // 여행 이름, 여행 국가, 기간 모두 다 입력하면 footer 색 변경
-    function updateFooterStyle() {
-        const isNameInputFilled = nameInput.value.trim() !== '';
-        const isNationSelected = lastSelectedNation !== null;
-        const isStartDateSelected = dateInputs[0].value !== '';
-        const isEndDateSelected = dateInputs[1].value !== '';
-        
-        const footer = document.querySelector('footer');
-        if (isNameInputFilled && isNationSelected && isStartDateSelected && isEndDateSelected) {
-            footer.style.backgroundColor = '#ED4B62';
-        } else {
-            footer.style.backgroundColor = '#F79FAB';
-        }
+  // 여행 이름, 여행 국가, 기간 모두 다 입력하면 footer 색 변경
+  function updateFooterStyle() {
+    const isNameInputFilled = nameInput.value.trim() !== "";
+    const isNationSelected = lastSelectedNation !== null;
+    const isStartDateSelected = dateInputs[0].value !== "";
+    const isEndDateSelected = dateInputs[1].value !== "";
+
+    const footer = document.querySelector("footer");
+    if (
+      isNameInputFilled &&
+      isNationSelected &&
+      isStartDateSelected &&
+      isEndDateSelected
+    ) {
+      footer.style.backgroundColor = "#ED4B62";
+    } else {
+      footer.style.backgroundColor = "#F79FAB";
     }
+  }
 });
 
 // range-bar와 #recent-money 연동 및 천 단위 구분 쉼표
 function updateMoneyValue(value) {
-    const formattedValue = Number(value).toLocaleString('en-US');
-    document.getElementById('recent-money').innerText = formattedValue;
+  const formattedValue = Number(value).toLocaleString("en-US");
+  document.getElementById("recent-money").innerText = formattedValue;
 }
